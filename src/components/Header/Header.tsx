45dbadf39919b2df7184ebb0f727e13161b8f83b
Brandon Blaylock <brandon@null.pub>
2020-05-17 12:47:27 -0700

    feat: switch to behive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import { h } from "preact";
 import { Link } from "preact-router/match";
-import { GiBee } from "react-icons/gi";
+import { GiTreeBeehive } from "react-icons/gi";
 
 export const Header = () => {
   return (
     <h1>
       <Link href="/" aria-label="Game list" class="fld-row flg-4">
         <span class="ct-rev-honey">
-          <GiBee />
+          <GiTreeBeehive />
         </span>
         <span>bee</span>
       </Link>
