263ac9e2c87b60fbf269a72f5b8dbb04dc95e5dd
Brandon Blaylock <brandon@null.pub>
2020-05-19 19:03:30 -0700

    fix: don't erase everyone's data

--- src/stores/game/validators.ts
+++ src/stores/game/validators.ts
@@ -12,9 +12,6 @@ export const GameStateCodec = C.type({
       date: C.string,
     })
   ),
-  settings: C.type({
-    vibration: C.boolean,
-  }),
 });
 export type GameStateCodec = C.TypeOf<typeof GameStateCodec>;
 
