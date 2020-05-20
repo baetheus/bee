bd4ada89dd7bd11a41a9ca26a5a987c3d528925e
Brandon Blaylock <brandon@null.pub>
2020-05-19 19:05:35 -0700

    fix: make settings a partial match on codec

--- src/stores/game/validators.ts
+++ src/stores/game/validators.ts
@@ -1,18 +1,25 @@
 import * as C from "io-ts/es6/Codec";
 import { Game } from "./models";
 
-export const GameStateCodec = C.type({
-  games: C.record(
-    C.type({
-      id: C.string,
-      chars: C.array(C.string),
-      middle: C.string,
-      dictionary: C.array(C.string),
-      found: C.array(C.string),
-      date: C.string,
-    })
-  ),
-});
+export const GameStateCodec = C.intersection(
+  C.type({
+    games: C.record(
+      C.type({
+        id: C.string,
+        chars: C.array(C.string),
+        middle: C.string,
+        dictionary: C.array(C.string),
+        found: C.array(C.string),
+        date: C.string,
+      })
+    ),
+  }),
+  C.partial({
+    settings: C.type({
+      vibration: C.boolean,
+    }),
+  })
+);
 export type GameStateCodec = C.TypeOf<typeof GameStateCodec>;
 
 export const GameCodec: C.Codec<Game> = C.type({
